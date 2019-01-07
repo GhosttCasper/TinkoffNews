ac154883aa393f07d161db8dd162763e80df80ee
Владимир Лаптев <vovalaptev.ru@mail.ru>
2019-01-07 16:07:38 +0700

    fix

--- app/src/main/java/com/example/laptop/tinkoffnews/MainActivity.kt
+++ app/src/main/java/com/example/laptop/tinkoffnews/MainActivity.kt
@@ -10,11 +10,14 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.zipWith
 import io.reactivex.schedulers.Schedulers
+import java.lang.RuntimeException
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var vText: TextView
-    var request:Disposable?=null
+    var request: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,30 +31,17 @@ class MainActivity : AppCompatActivity() {
             i.putExtra("tag1", vText.text)
             startActivityForResult(i, 0)
 
-            val o = Observable.create<String> {
-
-                it.onNext("adsf")
-            }.flatMap { Observable.create<String> {} }.zipWith(Observable.create<String> {})
+            val o = createRequest("").flatMap { Observable.create<String> {} }.zipWith(Observable.create<String> {})
                 .map { it.second + it.first }
                 .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 
-            request= o.subscribe({
+            request = o.subscribe({
 
 
             }, {
 
 
             })
-
-            /*val t = object : Thread() {
-                override fun run() {
-                    //super.run()
-                    this@MainActivity.runOnUiThread {}
-                }
-            }
-            t.start()
-
-            AT(this).execute()*/
         }
         Log.v("tag", "text")
     }
@@ -87,14 +77,3 @@ class MainActivity : AppCompatActivity() {
         request?.dispose()
     }
 }
-
-/*class AT(val act:MainActivity):AsyncTask<String, Int, String>() {
-    override fun doInBackground(vararg params: String?): String {
-        return "";
-    }
-
-    override fun onPostExecute(result: String?) {
-        super.onPostExecute(result)
-    }
-}
-*/
